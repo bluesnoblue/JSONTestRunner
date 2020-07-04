cabc9ac44ecbcb7d6d0186cbbfbac88d0b1f3b82
blues <470960070@qq.com>
2020-07-05 03:12:38 +0800

    add some demo testCase

--- demo-cases/test_Error.py
+++ demo-cases/test_Error.py
@@ -1,15 +1,17 @@
 import unittest
 
 
-class TestFoo(unittest.TestCase):
+class TestError(unittest.TestCase):
     """testFoo_doc"""
     @classmethod
     def setUpClass(cls) -> None:
-        print('TestFooSetUpClass')
+        print('TestErrorSetUpClass')
+        print(1+'')
 
     @classmethod
     def tearDownClass(cls) -> None:
         print('TestFooTearDownClass')
+        # print(1+'')
 
     def setUp(self) -> None:
         print('TestFooSetup')
@@ -17,15 +19,15 @@ class TestFoo(unittest.TestCase):
     def tearDown(self) -> None:
         print('TesrFooTearDown')
 
-    def test_foo_1(self):
-        """test_foo1_doc"""
-        print('test_foo1 running')
+    def test_error_1(self):
+        """test_error1_doc"""
+        print('test_error1 running')
         self.assertTrue(True)
         self.assertTrue(False)
 
-    def test_foo_2(self):
-        """test_foo2_doc"""
-        print('test_foo2 running')
+    def test_error_2(self):
+        """test_error2_doc"""
+        print('test_error2 running')
         print('a'+1)
         self.assertTrue(True)
         self.assertTrue(False)
